95629436ed90d9dcf3f0d3c244f91320082f1316
OlgaBogush <bohush.olga@gmail.com>
2025-12-09 01:16:13 +0300

    feat: add QuestionPage component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from "./components/MainLayout/MainLayout"
 import { BrowserRouter, Route, Routes } from "react-router"
 import HomePage from "./pages/HomePage/HomePage"
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"
+import QuestionPage from "./pages/QuestionPage/QuestionPage"
 
 const App = () => {
   return (
@@ -11,10 +12,7 @@ const App = () => {
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/addquestion" element={<div>addquestion</div>} />
-          <Route
-            path="/question/:id"
-            element={<div>single question page</div>}
-          />
+          <Route path="/question/:id" element={<QuestionPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
---
+++ src/components/Loader/SmallLoader.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+import style from "./SmallLoader.module.css"
+
+const SmallLoader = () => {
+  return <span className={style.loader}></span>
+}
+
+export default SmallLoader
