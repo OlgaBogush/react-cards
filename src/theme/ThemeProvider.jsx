a5965c8f982792f35927006ceba135e492152ec7
OlgaBogush <bohush.olga@gmail.com>
2026-01-13 16:15:03 +0300

    fix: correct ThemeProvider

--- src/theme/ThemeProvider.jsx
+++ src/theme/ThemeProvider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react"
+import React, { createContext, useLayoutEffect, useState } from "react"
 import { THEME_STORAGE } from "../constants"
 
 export const ThemeContext = createContext(null)
@@ -7,6 +7,7 @@ export const ThemeProvider = ({ children }) => {
   const savedTheme = localStorage.getItem(THEME_STORAGE) || "light"
 
   const [theme, setTheme] = useState(savedTheme)
+
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       {children}
